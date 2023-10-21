9f6ed143972cfd3db584ad3c21f0ce6b954bd51d
Prashant Garbuja <prashantmaha@yahoo.com>
2023-10-21 17:57:30 +1100

    Create facial_landmark_detection.py

---
+++ facial_landmark_detection.py
@@ -0,0 +1,27 @@
+import cv2
+import dlib
+
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+detector = dlib.get_frontal_face_detector()
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+
+    for face in faces:
+        landmarks = predictor(gray, face)
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
